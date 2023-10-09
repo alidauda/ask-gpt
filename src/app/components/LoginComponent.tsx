5c2bdf2d7a6541c7f70ac4d624b9fe2e26391e32
alidauda <alidauda14@gmail.com>
2023-10-09 21:04:19 +0100

    and the margin also

--- src/app/components/LoginComponent.tsx
+++ src/app/components/LoginComponent.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 import { Icons } from '@/helpers/Icons';
 export default function LoginComponent() {
   return (
-    <main className='grid h-[100dvh] place-content-center gap-3 bg-purple-300'>
+    <main className='grid h-[100dvh] place-content-center gap-3 bg-purple-300 m-8'>
       <div className='bg-white p-6 flex flex-col justify-center items-center gap-3 rounded shadow'>
         <h1 className='text-center font-medium  text-3xl'>Login</h1>
         <p className='text-center text-5xl font-normal'>welcome to chatWPdf</p>
